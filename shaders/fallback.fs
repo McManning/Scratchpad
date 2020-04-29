1130c82c2be3177f3f8e5dd918c85b5c21176274
Chase McManning <cmcmanning@gmail.com>
2020-04-28 22:18:42 -0400

    Add fallback shaders for live testing

--- shaders/fallback.fs
+++ shaders/fallback.fs
@@ -16,8 +16,14 @@ void main()
     vec3 eye = cameraPositionWS - IN.positionWS;
     float ndl = clamp(dot(IN.normalWS, normalize(eye)), 0.0, 1.0);
 
-    vec4 inner = vec4(0.5, 0.1, 0.1, 1);
-    vec4 outer = vec4(0.2, 0, 0, 1);
+    vec3 inner = vec3(0.61, 0.54, 0.52);
+    vec3 outer = vec3(0.27, 0.19, 0.18);
+    vec3 highlight = vec3(0.98, 0.95, 0.92);
+    
+    vec3 lit = vec3(0.61, 0.54, 0.52);
+    vec3 dark = vec3(0.24, 0.17, 0.16);
+    vec3 edge = vec3(0.19, 0.15, 0.15);
+    vec3 background = vec3(0.19, 0.19, 0.19);
 
-    FragColor = mix(outer, inner, ndl);
+    FragColor = vec4(mix(outer, mix(inner, highlight, ndl * 0.25), ndl * 0.75), 1);
 }
--- shaders/fallback.vs
+++ shaders/fallback.vs
@@ -1,8 +1,7 @@
 #version 330 core
 
-uniform mat4 MVP;
+uniform mat4 ModelViewProjectionMatrix;
 uniform mat4 ModelMatrix;
-uniform mat4 CameraMatrix;
 
 in vec3 Position;
 in vec3 Normal;
@@ -14,7 +13,7 @@ out VS_OUT {
 
 void main()
 {
-    gl_Position = MVP * vec4(Position, 1.0);
+    gl_Position = ModelViewProjectionMatrix * vec4(Position, 1.0);
     
     vec3 positionWS = (ModelMatrix * vec4(Position, 1.0)).xyz;
     vec3 normalWS = (ModelMatrix * vec4(Normal, 0)).xyz;
