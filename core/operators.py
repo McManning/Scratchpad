e8dd194f63a98861de72b2de3e797e91be0c19ca
Chase McManning <cmcmanning@gmail.com>
2020-07-06 18:44:56 -0400

    Refactor the force reload operator to apply to all materials

--- core/operators.py
+++ core/operators.py
@@ -6,13 +6,12 @@ from ..lib.registry import autoregister
 
 @autoregister
 class SCRATCHPAD_OT_reload_sources(Operator):
-    """Force reload of shader source files"""
+    """Force reload of all shader source files"""
     bl_idname = 'scratchpad.reload_sources'
     bl_label = 'Reload Shader Sources'
 
     def invoke(self, context, event):
-        # TODO: Refactor to trigger a reload for ALL materials
-        # TODO:MIGRATE
-        context.scene.scratchpad.force_reload = True
-        
+        for mat in bpy.data.materials:
+            mat.scratchpad.force_reload = True
+
         return {'FINISHED'}
