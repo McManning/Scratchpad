a16700e3f17352b05cc5efbe3a64303f77576889
Chase McManning <cmcmanning@gmail.com>
2020-07-29 21:42:58 -0400

    Simplify fallback shader

--- shaders/fallback.py
+++ shaders/fallback.py
@@ -8,54 +8,36 @@ VS_FALLBACK = '''
 #version 330 core
 
 uniform mat4 ModelViewProjectionMatrix;
-uniform mat4 ModelMatrix;
 
 in vec3 Position;
-in vec3 Normal;
 
 out VS_OUT {
-    vec3 positionWS;
-    vec3 normalWS;
+    vec3 position;
 } OUT;
 
 void main()
 {
     gl_Position = ModelViewProjectionMatrix * vec4(Position, 1.0);
-    
-    vec3 positionWS = (ModelMatrix * vec4(Position, 1.0)).xyz;
-    vec3 normalWS = (ModelMatrix * vec4(Normal, 0)).xyz;
-    
-    OUT.positionWS = positionWS;
-    OUT.normalWS = normalWS;
+    OUT.position = Position.xyz;
 }
 '''
 
 FS_FALLBACK = '''
 #version 330 core
 
-uniform mat4 CameraMatrix;
-
-layout (location = 0) out vec4 FragColor;
+out vec4 fragColor;
 
 in VS_OUT { 
-    vec3 positionWS;
-    vec3 normalWS;
+    vec3 position;
 } IN;
 
 void main()
 {
-    vec3 cameraPositionWS = CameraMatrix[3].xyz;
-
-    vec3 eye = cameraPositionWS - IN.positionWS;
-    float ndl = clamp(dot(IN.normalWS, normalize(eye)), 0.0, 1.0);
-    
-    vec3 inner = vec3(0.61, 0.54, 0.52);
-    vec3 outer = vec3(0.27, 0.19, 0.18);
-    vec3 highlight = vec3(0.98, 0.95, 0.92);
-    
-    FragColor = vec4(mix(outer, mix(inner, highlight, ndl * 0.25), ndl * 0.75), 1);
-    // FragColor = vec4(1, 0, 1, 1);
+    vec3 p = floor((IN.position.xyz + 0.00001) * 2.0);
+    float m = mod(p.x + p.y + p.z, 2.0);
+    fragColor = vec4(m, 0, m, 1);
 }
+
 '''
 
 class FallbackShader(BaseShader):
